57650c0f56267282dd6503f1a213c7c0bdc94545
Aws12505 <awsaljaramani12505@gmail.com>
2025-06-30 05:50:50 -0400

    .

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 
+Route::middleware(['excel_secret'])->group(function () {
 Route::post('/employees', [EmployeeController::class, 'store']);
+});
